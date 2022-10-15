a16168b78031930965b3c2408c671f372605bcc6
YannicFreson27 <yannic.freson1@gmail.com>
2022-10-15 14:39:36 +0200

    don't look plz

---
+++ firebaseInit.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAVd4TJRd4613Uic-zDny-fvyF6JY6fdeg",
+  authDomain: "artcard-820cd.firebaseapp.com",
+  databaseURL:
+    "https://artcard-820cd-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "artcard-820cd",
+  storageBucket: "artcard-820cd.appspot.com",
+  messagingSenderId: "468231970197",
+  appId: "1:468231970197:web:7cd9adb254c5bccfd6ea9c",
+};
+firebase.initializeApp(firebaseConfig);
