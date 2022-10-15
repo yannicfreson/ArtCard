6715ec7680e58d5315e73ae162119f69df8f5aaf
YannicFreson27 <yannic.freson1@gmail.com>
2022-10-15 12:11:26 +0200

    adding cards to db works

--- code.js
+++ code.js
@@ -8,13 +8,23 @@ database.ref().on("value", function (snapshot) {
 //set new card in db
 function addCardToDB(props) {
   const id = crypto.randomUUID();
+  console.log(props);
   database.ref(`cards/${id}`).push({
     id: id,
     date: new Date().toISOString(),
     userName: props.userName,
     userAddress: props.userAddress,
-    museumAddress: props.museumAddress,
     museumName: props.museumName,
-    imageUrls: props.imageUrls,
+    imageUrls: [],
   });
+}
+
+function parseForm() {
+  let props = {
+    userName: document.getElementById("userName").value,
+    userAddress: document.getElementById("userAddress").value,
+    museumName: document.getElementById("museumName").value,
+    userMessage: document.getElementById("userMessage").value,
+  };
+  addCardToDB(props);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["index.html", "code.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#0b1120",
+        box: "#1e293b",
+        boxLighter: "#2d3748",
+        accent: "#38bdf8",
+        subtext: "#64748b",
+        textbox: "#0e1527",
+      },
+    },
   },
   plugins: [
     function ({ addVariant }) {
